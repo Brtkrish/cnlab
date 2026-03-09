2b5534eda5444e13d4d95a42d12b63ccfc5afa17
Tannie16 <149944641+Tannie16@users.noreply.github.com>
2026-03-09 20:31:58 +0530

    Refactor FTP server to handle client requests

--- ftpserver.c
+++ ftpserver.c
@@ -1,81 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
-#define BUFFER_SIZE 1000
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void handle_client(int client_socket) {
+    char buffer[BUFFER_SIZE] = {0};
+    char filename[BUFFER_SIZE] = {0};
+
+    int bytes_received = recv(client_socket, filename, BUFFER_SIZE - 1, 0);
+    if (bytes_received < 0) {
+        perror("Error receiving filename");
+        close(client_socket);
+        return;
+    }
+
+    filename[strcspn(filename, "\r\n")] = 0;
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        char *error_message = "File not found";
+        send(client_socket, error_message, strlen(error_message), 0);
+        perror("File not found");
+    } else {
+        while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+            send(client_socket, buffer, strlen(buffer), 0);
+            memset(buffer, 0, BUFFER_SIZE);
+        }
+        printf("File sent to client\n");
+        fclose(file);
+    }
+    close(client_socket);
+}
 
 int main() {
-    int server_fd, new_socket, valread, port;
-    struct sockaddr_in address;
-    socklen_t addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    char *error_msg = "error";
-    char *completed_msg = "completed";
-    FILE *fp;
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
 
-    printf("Enter a port number: ");
-    scanf("%d", &port);
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("socket failed");
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
+    int opt = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen failed");
+    if (listen(server_socket, 3) < 0) {
+        perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Server is listening on port %d...\n", port);
+    printf("Server is listening on port %d\n", PORT);
 
     while (1) {
-        new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
-        if (new_socket < 0) {
-            perror("accept failed");
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_socket < 0) {
+            perror("Accept failed");
             continue;
         }
-        printf("Connection established...\n");
-
-        memset(buffer, 0, BUFFER_SIZE);
-        valread = recv(new_socket, buffer, BUFFER_SIZE, 0);
-        if (valread < 0) {
-            send(new_socket, error_msg, strlen(error_msg), 0);
-            close(new_socket);
-            continue;
-        }
-
-        buffer[valread] = '\0';
-        printf("Client requests file: %s\n", buffer);
-
-        fp = fopen(buffer, "rb");
-        if (fp == NULL) {
-            send(new_socket, error_msg, strlen(error_msg), 0);
-        } else {
-            char file_buffer[BUFFER_SIZE];
-            int bytes_read;
-            while ((bytes_read = fread(file_buffer, 1, BUFFER_SIZE, fp)) > 0) {
-                send(new_socket, file_buffer, bytes_read, 0);
-            }
-            fclose(fp);
-            send(new_socket, completed_msg, strlen(completed_msg), 0);
-        }
-
-        close(new_socket);
-        printf("Session completed. Listening for new connection...\n");
+        printf("Client connected\n");
+        handle_client(client_socket);
     }
 
+    close(server_socket);
     return 0;
 }
