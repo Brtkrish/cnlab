4add4d8c2047f85a582f04adda7d5e3c12b98617
Tannie16 <149944641+Tannie16@users.noreply.github.com>
2026-03-09 20:57:20 +0530

    Refactor Dijkstra's algorithm implementation-LSR

--- lsr.c
+++ lsr.c
@@ -3,44 +3,48 @@
 #define MAX 10
 #define INF 999
 
-int main()
-{
-    int n, cost[MAX][MAX], dist[MAX], visited[MAX];
+void printPath(int parent[], int j) {
+    if (parent[j] == -1)
+        return;
+    printPath(parent, parent[j]);
+    printf(" -> %d", j);
+}
+
+int main() {
+    int n, cost[MAX][MAX], dist[MAX], visited[MAX], parent[MAX];
     int i, j, min, next, src;
 
     printf("Enter number of routers: ");
     scanf("%d", &n);
 
     printf("Enter the cost matrix:\n");
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-        {
-            scanf("%d",&cost[i][j]);
-            if(cost[i][j]==0 && i!=j)
-                cost[i][j]=INF;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &cost[i][j]);
+            if (cost[i][j] == 0 && i != j)
+                cost[i][j] = INF;
         }
+    }
 
     printf("Enter source router: ");
-    scanf("%d",&src);
+    scanf("%d", &src);
 
-    for(i=0;i<n;i++)
-    {
+    for (i = 0; i < n; i++) {
         dist[i] = cost[src][i];
         visited[i] = 0;
+        if (dist[i] < INF && i != src)
+            parent[i] = src;
+        else
+            parent[i] = -1;
     }
 
     dist[src] = 0;
     visited[src] = 1;
 
-    /* Dijkstra Algorithm */
-    for(i=0;i<n-1;i++)
-    {
+    for (i = 0; i < n - 1; i++) {
         min = INF;
-
-        for(j=0;j<n;j++)
-        {
-            if(!visited[j] && dist[j] < min)
-            {
+        for (j = 0; j < n; j++) {
+            if (!visited[j] && dist[j] < min) {
                 min = dist[j];
                 next = j;
             }
@@ -48,19 +52,21 @@ int main()
 
         visited[next] = 1;
 
-        for(j=0;j<n;j++)
-        {
-            if(!visited[j] && (min + cost[next][j] < dist[j]))
-            {
+        for (j = 0; j < n; j++) {
+            if (!visited[j] && (min + cost[next][j] < dist[j])) {
                 dist[j] = min + cost[next][j];
+                parent[j] = next;
             }
         }
     }
 
-    printf("\nShortest distance from router %d:\n", src);
-
-    for(i=0;i<n;i++)
-        printf("Router %d -> Router %d = %d\n", src, i, dist[i]);
+    printf("\nShortest paths from router %d:\n", src);
+    for (i = 0; i < n; i++) {
+        if (i == src) continue;
+        printf("To %d: Distance = %d, Path = %d", i, dist[i], src);
+        printPath(parent, i);
+        printf("\n");
+    }
 
     return 0;
 }
