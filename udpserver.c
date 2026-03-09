1f9b58cfd380474319f13859bb76493a701a59b5
Tannie16 <149944641+Tannie16@users.noreply.github.com>
2026-03-09 20:30:18 +0530

    Refactor UDP server implementation

--- udpserver.c
+++ udpserver.c
@@ -1,55 +1,49 @@
-// UDP Server
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <unistd.h>
 
 int main() {
-    int server;
-    char servMsg[2000], cliMsg[2000];
-    struct sockaddr_in servAddr, client_addr;
-    socklen_t client_struct_length = sizeof(client_addr);   
+    int serversocket, port;
+    struct sockaddr_in serveraddr, clientaddr;
+    socklen_t len;
+    char message[1024];
 
-    server = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (server < 0) {
-        printf("Error while creating socket\n");
+    serversocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (serversocket < 0) {
+        perror("Socket failed");
         exit(1);
     }
-    printf("1. Socket created successfully\n");
 
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(2002);
-    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
-        printf("Couldn't bind to the port\n");
+    printf("Enter the port number: ");
+    scanf("%d", &port);
+    serveraddr.sin_port = htons(port);
+
+    if (bind(serversocket, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0) {
+        perror("Bind failed");
         exit(1);
     }
-    printf("2. Binding done\n");
-    printf("3. Listening...\n");
 
-    memset(cliMsg, 0, sizeof(cliMsg));   
-    if (recvfrom(server, cliMsg, sizeof(cliMsg), 0,
-        (struct sockaddr*)&client_addr, &client_struct_length) < 0) {
-        printf("Couldn't receive\n");
-        exit(1);
+    len = sizeof(clientaddr);
+    printf("\nWaiting for message from client...\n");
+
+    memset(message, 0, sizeof(message));
+    ssize_t n = recvfrom(serversocket, message, sizeof(message) - 1, 0, (struct sockaddr*)&clientaddr, &len);
+
+    if (n > 0) {
+        message[n] = '\0';
+        printf("Message received from client: %s\n", message);
+
+        const char *response = "HELLO CLIENT...";
+        sendto(serversocket, response, strlen(response), 0, (struct sockaddr*)&clientaddr, len);
     }
-    printf("4. Message from client: %s\n", cliMsg);
 
-    memset(servMsg, 0, sizeof(servMsg));   
-    strcpy(servMsg, cliMsg);
-    if (sendto(server, servMsg, strlen(servMsg), 0,
-        (struct sockaddr*)&client_addr, client_struct_length) < 0) {
-        printf("Can't send\n");
-        exit(1);
-    }
-    printf("5. Message sent back to client\n");
-
-    close(server);
-    printf("6. Socket closed\n");
-
-    return 0; 
+    close(serversocket);
+    return 0;
 }
