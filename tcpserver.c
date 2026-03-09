da9d34a172b5ba49b0eba9566bb2d21b7f50bed4
Tannie16 <149944641+Tannie16@users.noreply.github.com>
2026-03-09 19:51:29 +0530

    Update tcpserver.c

--- tcpserver.c
+++ tcpserver.c
@@ -1,68 +1,50 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
-    int server, newSock;
-    char buffer[1024];
-    struct sockaddr_in servAddr;
-    struct sockaddr_storage store;
-    socklen_t addrSize;
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
+    char response[BUFFER_SIZE + 32];
 
-    server = socket(AF_INET, SOCK_STREAM, 0);
-    if (server < 0) {
-        perror("Socket creation failed");
-        return 1;
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(server_fd, 3);
+
+    printf("TCP Server listening on port %d...\n", PORT);
+
+    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+    printf("Client connected.\n");
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+        
+        if (bytes_received <= 0) break;
+
+        buffer[strcspn(buffer, "\n")] = 0;
+        printf("Client: %s\n", buffer);
+
+        snprintf(response, sizeof(response), "received \"%s\"\n", buffer);
+        printf("Server: %s", response);
+        send(client_fd, response, strlen(response), 0);
     }
-    printf("1. Socket created successfully.\n");
 
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(6265);
-    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if (bind(server, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
-        perror("Bind failed");
-        return 1;
-    }
-    printf("2. Bind successful.\n");
-
-    if (listen(server, 5) == 0)
-        printf("3. Listening on port 6265...\n");
-    else {
-        perror("Listen failed");
-        return 1;
-    }
-
-    addrSize = sizeof(store);
-    newSock = accept(server, (struct sockaddr*)&store, &addrSize);
-    if (newSock < 0) {
-        perror("Accept failed");
-        return 1;
-    }
-    printf("4. Client connected.\n");
-
-    memset(buffer, 0, sizeof(buffer));
-    if (recv(newSock, buffer, sizeof(buffer), 0) < 0) {
-        perror("Receive failed");
-        return 1;
-    }
-    printf("5. Data received from client: %s", buffer);
-
-    memset(buffer, 0, sizeof(buffer));
-    strcpy(buffer, "Hi! This is Server. Message received.\n");
-    printf("6. Sending data to client...\n");
-    if (send(newSock, buffer, strlen(buffer), 0) < 0) {
-        perror("Send failed");
-        return 1;
-    }
-    printf("7. Data sent successfully.\n");
-
-    close(newSock);
-    close(server);
-    printf("8. Connection closed.\n");
-
+    close(client_fd);
+    close(server_fd);
     return 0;
 }
